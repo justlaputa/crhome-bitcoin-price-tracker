da4a2793fb5032d36ff244a31e2ec645ae845f55
Xiao HAN <han_x@worksap.co.jp>
2018-01-18 20:47:38 +0900

    add one api

--- bitflyer-api.js
+++ bitflyer-api.js
@@ -3,12 +3,25 @@
 import request from 'request'
 import crypto from 'crypto'
 
-class Bitflyer {
-    constructor() {
+const API_ENDPOINTS = {
+    BOARD: {
+        method: 'POST',
+        path: '/board'
+    }
+}
 
+class Bitflyer {
+    constructor(key, secret) {
+        this.apiHostUrl = 'https://api.bitflyer.jp/v1/'
+        this.apiKey = key
+        this.apiSecret = secret
     }
 
     getboard() {
+        call(API_ENDPOINTS.BOARD)
+    }
+
+    call(endpoint) {
 
     }
 }
